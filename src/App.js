011411e4af5841e7044fbbdc7ed0bc71cc1c4a5b
gudwnsgur <gudwnsgur12@naver.com>
2019-07-03 15:07:25 +0900

    modify App.js

--- src/App.js
+++ src/App.js
@@ -2,54 +2,58 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
+import { taggedTemplateExpression } from '@babel/types';
 
 class App extends Component {
 
   // componentWillMount() {}
   state = {
-    movies : [
-      {
-        title : "Iron Man",
-        poster : "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG"
-      },
-      {
-        title : "Thor",
-        poster : "https://upload.wikimedia.org/wikipedia/en/f/fc/Thor_poster.jpg"
-      },
-      {
-        title : "Spider Man",
-        poster : "https://images-na.ssl-images-amazon.com/images/I/71ZKGZ0fWfL._SY606_.jpg"
-      },
-      {
-        title : "Doctor Strange",
-        poster : "https://images-na.ssl-images-amazon.com/images/I/61yHlaxjkgL.jpg"
-      }
-    ]
   }
 
   componentDidMount () {
     setTimeout(() => {
       this.setState ({
         movies : [
-          ...this.state.movies, {
+          {
+            title : "Iron Man",
+            poster : "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG"
+          },
+          {
+            title : "Thor",
+            poster : "https://upload.wikimedia.org/wikipedia/en/f/fc/Thor_poster.jpg"
+          },
+          {
+            title : "Spider Man",
+            poster : "https://images-na.ssl-images-amazon.com/images/I/71ZKGZ0fWfL._SY606_.jpg"
+          },
+          {
+            title : "Doctor Strange",
+            poster : "https://images-na.ssl-images-amazon.com/images/I/61yHlaxjkgL.jpg"
+          },
+          {
             title : "Captain America",
             poster : "https://images-na.ssl-images-amazon.com/images/I/71krfsS5kNL._SY679_.jpg"
           }
         ]
       })
-    }, 3000);
+    }, 5000)
   }
 
-  render () {
-  return (
-    <div className="App">
-      {this.state.movies.map((movie, index) => {
-        return <Movie title={movie.title} poster={movie.poster} key={index}/> 
-      })}
-    </div>
-  );
-}
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/> 
+    })
+    return movies
+  }
 
+
+  render () {
+    return (
+      <div className="App"> 
+        {this.state.movies ? this._renderMovies() : 'Loading'}
+      </div>
+    );
+  }
 }
 
 
