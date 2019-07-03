2608e712adb41ac19e580e73deddb5ea0e8dac35
gudwnsgur <gudwnsgur12@naver.com>
2019-07-03 13:33:04 +0900

    modify Movie.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Iron Man",
-  "Thor",
-  "Spider-Man",
-  "Doctor Strange"
-]
-const movieImages = [
-  "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG",
-  "https://upload.wikimedia.org/wikipedia/en/f/fc/Thor_poster.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/71ZKGZ0fWfL._SY606_.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/61yHlaxjkgL.jpg"
-]
+class App extends Component {
 
-function App() {
+  // componentWillMount() {}
+  state = {
+    movies : [
+      {
+        title : "Iron Man",
+        poster : "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG"
+      },
+      {
+        title : "Thor",
+        poster : "https://upload.wikimedia.org/wikipedia/en/f/fc/Thor_poster.jpg"
+      },
+      {
+        title : "Spider Man",
+        poster : "https://images-na.ssl-images-amazon.com/images/I/71ZKGZ0fWfL._SY606_.jpg"
+      },
+      {
+        title : "Doctor Strange",
+        poster : "https://images-na.ssl-images-amazon.com/images/I/61yHlaxjkgL.jpg"
+      }
+    ]
+  }
+
+  componentDidMount () {
+    setTimeout(() => {
+      this.setState ({
+        movies : [
+          ...this.state.movies, {
+            title : "Captain America",
+            poster : "https://images-na.ssl-images-amazon.com/images/I/71krfsS5kNL._SY679_.jpg"
+          }
+        ]
+      })
+    }, 3000);
+  }
+
+  render () {
   return (
     <div className="App">
-      <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-      <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-      <Movie title={movieTitles[2]} poster={movieImages[2]}/>
-      <Movie title={movieTitles[3]} poster={movieImages[3]}/>
+      {this.state.movies.map((movie, index) => {
+        return <Movie title={movie.title} poster={movie.poster} key={index}/> 
+      })}
     </div>
   );
 }
 
+}
+
 
 export default App;
+// render->return | props | map | prop-types | life-cycle | state
+
+// lifecycle : (1) componentWillMount() -> (2) render() -> (3) componentDidMount() 
+// update : componentWillErceiveProps() -> shouldComponentUpdate() -> componentWillUpdate()
+//          -> render() -> component
