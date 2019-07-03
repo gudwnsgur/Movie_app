8f478ea4031e0afa7961273e470bc91bc8031101
gudwnsgur <gudwnsgur12@naver.com>
2019-07-03 09:28:05 +0900

    start making Movie.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+
+const movieTitles = [
+  "Iron Man",
+  "Thor",
+  "Spider-Man",
+  "Doctor Strange"
+]
+const movieImages = [
+  "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG",
+  "https://upload.wikimedia.org/wikipedia/en/f/fc/Thor_poster.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/71ZKGZ0fWfL._SY606_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/61yHlaxjkgL.jpg"
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Movie title={movieTitles[0]} poster={movieImages[0]}/>
+      <Movie title={movieTitles[1]} poster={movieImages[1]}/>
+      <Movie title={movieTitles[2]} poster={movieImages[2]}/>
+      <Movie title={movieTitles[3]} poster={movieImages[3]}/>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,24 @@
+import React, {Component}  from 'react';
+import './Movie.css';
+
+class Movie extends Component {
+    render () {
+        // console.log(this.props);
+        return (
+        <div>
+            <MoviePoster poster={this.props.poster}/>
+            <h1>{this.props.title}</h1>
+        </div>
+        );
+    }
+}
+
+class MoviePoster extends Component {
+    render () {
+        return (
+            <img src={this.props.poster} />
+        );
+    }
+}
+
+export default Movie;
